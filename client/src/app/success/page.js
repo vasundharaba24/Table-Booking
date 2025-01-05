dce897bcc400f5f7e7446b3e306f7649abb1c5ff
vasundharaba24 <85022737+vasundharaba24@users.noreply.github.com>
2025-01-05 19:43:17 +0530

    Update page.js

--- client/src/app/success/page.js
+++ client/src/app/success/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function SuccessPage() {
   const searchParams = useSearchParams();
@@ -22,11 +23,15 @@ export default function SuccessPage() {
   }, [router]);
 
   return (
+  <Suspense fallback={<div>Loading...</div>}>
+    
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <h1>Table Booking Successful!</h1>
       <p>Thank you, <strong>{name}</strong>, for booking a table with us!</p>
       <p>Your reservation is on <strong>{date}</strong> at <strong>{time}</strong>.</p>
       <p>You will be redirected to the homepage in 10 seconds...</p>
     </div>
+  
+</Suspense>
   );
 }
