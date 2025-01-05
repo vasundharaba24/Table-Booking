fc078f458d90bbfd4a86e53657f43fc0f96ab093
vasundharaba24 <85022737+vasundharaba24@users.noreply.github.com>
2025-01-06 04:38:04 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     eslint:{
         ignoreDuringBuilds: true,
     },
+    async redirects() {
+    return [
+      {
+        source: '/book-table',
+        destination: 'https://your-backend.com/book-table',
+        permanent: false, // Use true for 308 permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
