0a052eab10e7eb87c92a5363953b3994942dee2d
vasundharaba24 <85022737+vasundharaba24@users.noreply.github.com>
2025-01-06 04:29:18 +0530

    Update page.js

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -7,7 +7,7 @@ export default function HomePage() {
   const router = useRouter();
 
   const handleBookTable = () => {
-    router.push('https://table-booking-s47e.onrender.com/book-table'); // Navigate to the book table page
+    router.push('https://table-booking-hzcopvvm2-vasundharaba24s-projects.vercel.app/book-table'); // Navigate to the book table page
   };
 
   return (
