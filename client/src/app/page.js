0dede5e0e96c168758c0ab59aa44b3f3a4c58aa7
vasundharaba24 <85022737+vasundharaba24@users.noreply.github.com>
2025-01-06 04:54:33 +0530

    Update page.js

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -7,7 +7,7 @@ export default function HomePage() {
   const router = useRouter();
 
   const handleBookTable = () => {
-    router.push('/book-table'); // Navigate to the book table page
+    router.push('https://vercel.com/vasundharaba24s-projects/table-booking/5nstZu8bpGpUKwCBzJYDyM9UWQ9q/book-table'); // Navigate to the book table page
   };
 
   return (
@@ -18,7 +18,7 @@ export default function HomePage() {
       {/* Content */}
       <div className="content">
         <h1>Welcome to Cafe De Paris</h1>
-        <Link href="/book-table" classname="button">Book Table</Link>
+        <Link classname="button">Book Table</Link>
 
       </div>
     </div>
